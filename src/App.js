a16f8ffb05ec78cc61c9059676e132e2d565d18c
Amber-14 <amberyadav311@gmail.com>
2022-04-02 21:40:56 +0530

    markdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       text shree
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      markdown:''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(e){
+    this.setState({
+      markdown: e.target.value
+    })
+  }
+  
+  render() {
+    return (
+      <div className='app'>
+      <textarea onChange={this.handleChange} />
+      <ReactMarkdown className='preview' children={this.state.markdown}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
